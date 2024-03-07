cb2439cbd6751815e8864f13ffd484a7f3fa1feb
Jacob <Arthursjacob01@gmail.com>
2024-03-06 18:17:03 -0600

    Add start and end date to product search

--- products/queries.py
+++ products/queries.py
@@ -3,6 +3,7 @@ from django.db.models import Count
 from django.db.models.functions import TruncMonth
 from django.utils.timezone import now
 from dateutil.relativedelta import relativedelta
+from datetime import datetime, time
 from .types import ProductType, ProductsPerMonthType
 from .models import Product
 
@@ -24,6 +25,8 @@ class ProductQuery(graphene.ObjectType):
         max_cost=graphene.Float(default_value=None, description="The maximum cost of products to retrieve."),
         min_supply=graphene.Int(default_value=None, description="The minimum supply of products to retrieve."),
         max_supply=graphene.Int(default_value=None, description="The maximum supply of products to retrieve."),
+        start_date=graphene.Date(default_value=None, description="The start date of products to retrieve."),
+        end_date=graphene.Date(default_value=None, description="The end date of products to retrieve."),
         description="Search for products based on various criteria such as name, description, cost range, and supply range."
     )
     products_per_month = graphene.List(
@@ -70,6 +73,12 @@ class ProductQuery(graphene.ObjectType):
             queryset = queryset.filter(supply__gte=kwargs['min_supply'])
         if kwargs.get('max_supply') is not None:
             queryset = queryset.filter(supply__lte=kwargs['max_supply'])
+        if kwargs.get('start_date') is not None:
+            start_datetime = datetime.combine(kwargs['start_date'], time.min)
+            queryset = queryset.filter(created_at__gte=start_datetime)
+        if kwargs.get('end_date') is not None:
+            end_datetime = datetime.combine(kwargs['end_date'], time.max)
+            queryset = queryset.filter(created_at__lte=end_datetime)
         
         return queryset
     
@@ -92,5 +101,5 @@ class ProductQuery(graphene.ObjectType):
         
         product_count_dict = {item['month'].strftime("%B %Y"): item['product_count'] for item in queryset}
         months_in_range = [(start_date + relativedelta(months=i)).strftime("%B %Y") for i in range(last_n_months)]
-        
+
         return [ProductsPerMonthType(month=month, product_count=product_count_dict.get(month, 0)) for month in months_in_range]
