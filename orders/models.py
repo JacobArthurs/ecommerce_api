8f19b86d92a2dfae475f418f7112669450f65022
Jacob <Arthursjacob01@gmail.com>
2024-03-30 18:27:48 -0500

    Alter order model

--- orders/models.py
+++ orders/models.py
@@ -3,7 +3,9 @@ from django.contrib.auth.models import User
 from products.models import Product
 
 class Order(models.Model):
+    total_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
@@ -12,7 +14,9 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='order_items')
     quantity = models.PositiveIntegerField(default=1)
-    price_at_time_of_order = models.DecimalField(max_digits=10, decimal_places=2)
+    cost = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"{self.quantity} of {self.product.name} in Order {self.order.id}"
