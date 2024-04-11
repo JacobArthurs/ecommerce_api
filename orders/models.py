a7194543795d8fb143d9b2ac3491e6f7851ce490
Jacob <Arthursjacob01@gmail.com>
2024-04-10 19:25:25 -0500

    Add user to models

--- orders/models.py
+++ orders/models.py
@@ -6,6 +6,7 @@ class Order(models.Model):
     total_cost = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
 
     def __str__(self):
         return f"Order {self.id} by {self.user.username}"
